8525fe248f8693f61951ba82249570e080b20813
F1LT3R <al@pwn.io>
2016-12-20 15:50:01 -0500

    Robonaut can git clone and npm install

--- robonaut.js
+++ robonaut.js
@@ -1,23 +1,36 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
+var fs = require('fs-extra')
 const path = require('path');
+const childProcess = require('child_process');
 
 const chalk = require('chalk');
 const jsonfile = require('jsonfile');
+const packageJson = require('package-json');
+const gitRevSync = require('git-rev-sync');
 
 jsonfile.spaces = 4;
 
-const strap = chalk.bgYellow.black.bold('[:|]');
+// 📡🛰👽👾🎮📦☄🌚
+
+const strap = chalk.bgGreen.black.bold(' Robonaut ');
 const cwdRoot = process.cwd();
 
 const log = (msg, col) => {
-	console.log(`${strap} ${chalk[col || 'white'](msg)}`);
+	let logMsg;
+	if (typeof msg === 'string') {
+		logMsg = `${strap} ${chalk[col || 'white'](msg)}`;
+	} else if (typeof msg === 'object') {
+		logMsg = `${strap} ${chalk[col || 'white'](JSON.stringify(msg))}`;
+	}
+
+	console.log(logMsg);
+
+	return logMsg;
 };
 
-const showHelp = flag => {
+const showHelp = () => {
 	log('robonaut help');
-	console.log(flag);
 };
 
 // const fileExistsSync = path => {
@@ -114,22 +127,169 @@ const init = () => {
 	}
 };
 
-const gather = deps => {
+const skyjack = deps => {
 	const roboDeps = deps[0].split(',');
 
-	log(`gather ${JSON.stringify(roboDeps)}`);
+	log(`skyjack: ${JSON.stringify(roboDeps)}`);
 
 	const pkg = getPkg();
 
-	console.log(pkg.data);
+	let depsAdded = 0;
+
+	if (!Reflect.has(pkg, 'data') || !Reflect.has(pkg.data, 'robonautDeps')) {
+		throw log(`skyjack: failed, no robonautDeps in package (try 'robonaut init')`);
+	}
 
 	for (dep of roboDeps) {
 		if (pkg.data.robonautDeps.indexOf(dep) === -1) {
 			pkg.data.robonautDeps.push(dep);
+			log(`skyjack: '${chalk.underline(dep)}' has been skyjacked`);
+			depsAdded += 1;
 		}
 	}
 
-	saveJson(pkg.path, pkg.data);
+	if (depsAdded > 0) {
+		saveJson(pkg.path, pkg.data);
+	} else {
+		log(`skyjack: no new deps added`);
+	}
+};
+
+const assemble = () => {
+	decimate();
+
+	const pkg = getPkg();
+
+	if (!Reflect.has(pkg, 'data') ||
+		!Reflect.has(pkg.data, 'robonautDeps') ||
+		pkg.data.robonautDeps.length < 1) {
+		throw log(`assembled: failed, no robonautDeps in package (try 'robonaut init')`);
+	}
+
+	const roboModsDir = path.join(cwdRoot, 'robonaut_modules');
+
+	let cloned = 0;
+	let installed = 0;
+	let depCount = pkg.data.robonautDeps.length;
+
+	const robonautDeps = pkg.data.robonautDeps;
+	const depStack = {};
+
+	const logAssembleResults = () => {
+		// log(chalk.yellow.underline('ASSEMBLED...'));
+		log(`assemble: ${chalk.cyan('ALL PACKAGES ASSEMBLED OK ✔  ')}`);
+		// spawnNpmInstall(def);
+
+		log('🏁  ' + chalk.yellow.underline('ASSEMBLED...'));
+		for (name of robonautDeps) {
+			const def = depStack[name];
+			log(`${chalk.green(def.name)}: ${chalk.blue('  ')}@${def.gitHash} ${chalk.blue('  ')}@${def.version}  ${chalk.blue('✔')} `);
+		};
+
+	};
+
+	const logFinishedClones = () => {
+		log('🏁  ' + chalk.yellow.underline('CLONED...'));
+		for (name of robonautDeps) {
+			const def = depStack[name];
+			const gitHash = gitRevSync.short(def.dir);
+			def.gitHash = gitHash;
+			log(`${chalk.green(name)} @${gitHash}`);
+		};
+	};
+
+	const logFinishedInstalls = () => {
+		log('🏁  ' + chalk.yellow.underline('INSTALLED...'));
+		for (name of robonautDeps) {
+			const version = depStack[name].version;
+			log(`${chalk.green(name)} @${version}`);
+		};
+		logAssembleResults();
+	};
+
+	const spawnNpmInstall = name => {
+		const def = depStack[name];
+
+		def.npmInstallOpts = {
+			cwd: def.dir,
+			// detached: true,
+			stdio: ['ignore', 1, 2]
+		};
+
+		def.npmInstallArgs = ['install'];
+		def.npmInstall = childProcess.spawn('npm', def.npmInstallArgs, def.npmInstallOpts);
+
+		log(`assemble: 📡  ${chalk.yellow('npm install ' + name)}`);
+
+		def.npmInstall.on('close', code => {
+			// log(`assemble: npm install 📦  ${chalk.green(def.name)} ${chalk.cyan('✔')} `);
+			log(`assemble: npm install ${chalk.cyan('')}  ${chalk.green(def.name)} ${chalk.cyan('✔')} `);
+
+			installed += 1;
+
+			if (installed === depCount) {
+				logFinishedInstalls();
+			}
+		});
+	};
+
+	const queueInstalls = () => {
+		for (depName of robonautDeps) {
+			spawnNpmInstall(depName);
+		}
+	};
+
+	const spawnGitClone = json => {
+		const name = json.name;
+		const def = depStack[name];
+
+		def.dir = path.join(roboModsDir, json.name);
+		def.repo = json.repository.url.replace('git+', '');
+		def.version = json.version;
+		def.json = json;
+
+		def.gitCloneOpts = {
+			cwd: roboModsDir,
+			stdio: ['ignore', 1, 2]
+		};
+
+		def.gitCloneArgs = ['clone', def.repo];
+		def.gitClone = childProcess.spawn('git', def.gitCloneArgs, def.gitCloneOpts);
+
+		log(`assemble: 📡  ${chalk.yellow('git clone ' + json.name)}`);
+
+		def.gitClone.on('close', code => {
+			cloned += 1;
+
+			const doneMsg = `assemble: git clone ${chalk.cyan('')}  ${chalk.green(def.name)} ${chalk.cyan('✔')}`;
+			log(doneMsg);
+
+			if (cloned === depCount) {
+				logFinishedClones();
+				queueInstalls();
+			}
+		});
+	};
+
+	const fetchPackages = () => {
+		for (depName of robonautDeps) {
+			const depDef = {
+				name: depName,
+			};
+
+			depStack[depName] = depDef;
+
+			packageJson(depName, 'latest')
+			.then(function (json) {
+				spawnGitClone(json);
+			})
+			.catch(err => {
+				throw err;
+			});
+		}
+	};
+
+	fetchPackages();
 };
 
 const crosslink = props => {
@@ -145,19 +305,30 @@ const exec = props => {
 };
 
 const prime = props => {
+	//git status -s
 };
 
 const transmit = props => {
 };
 
+const decimate = () => {
+	log(`🔥  decimating...`);
+	const roboModsDir = path.join(cwdRoot, 'robonaut_modules');
+	fs.removeSync(roboModsDir);
+	fs.mkdirSync(roboModsDir);
+	log(`decimate: ${chalk.green(roboModsDir)} ${chalk.bgGreen.black(' DECIMATED OK ')} 👍  `);
+};
+
 const main = {
 	init,
-	gather,
+	skyjack,
+	assemble,
 	crosslink,
 	npm,
 	git,
 	exec,
 	prime,
+	decimate,
 	transmit
 };
 
@@ -175,12 +346,20 @@ const stackCmds = args => {
 
 const processCmds = cmds => {
 	log('Process Cmds');
+
+	let validCmds = 0;
+
 	for (let cmd of cmds) {
 		const cmdName = cmd.shift();
 
 		if (Reflect.has(main, cmdName)) {
+			validCmds += 1;
 			main[cmdName](cmd);
 		}
+	}
+
+	if (validCmds === 0) {
+		showHelp();
 	}
 };
 
