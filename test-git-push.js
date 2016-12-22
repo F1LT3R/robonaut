969edaa40267f321c422beeac17cafbb1ca560a9
F1LT3R <al@pwn.io>
2016-12-22 18:34:26 -0500

    TEst

--- test-git-push.js
+++ test-git-push.js
@@ -5,4 +5,20 @@ const child = childProcess.spawn('git', ['push', 'origin', 'master', '-v'], {
 	stdio: 'inherit'
 });
 
+child.on('error', err => {
+	console.log('ERROR');
+	console.error(err);
+});
+
+child.on('close', code => {
+	console.log('CLOSE');
+	console.log(code);
+});
+
+child.on('exit', code => {
+	console.log('EXIT');
+	console.log(code);
+});
+
+
 
